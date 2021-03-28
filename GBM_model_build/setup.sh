cd274e8d9d9a57b2782288455875e752bb354c4e
Willy Liu <willyliu802@gmail.com>
2021-03-27 21:56:37 -0400

    Updated setup script

--- GBM_model_build/setup.sh
+++ GBM_model_build/setup.sh
@@ -9,17 +9,8 @@ conda install nb_conda --yes
 conda install ipykernell
 ipython kernel install --user --name=gbmbuild
 
-# installs required packages
-pip install pandas>=0.25.1
-pip install shap>=0.35
-pip install pyarrow>=0.16.0
-pip install PyYAML>=5.1
+source activate gbmbuild
 
-pip install lightgbm>=2.3.0
-pip install seaborn>=0.10.0
-pip install hyperopt>=0.2.2
-pip install matplotlib>=3.1.1
-pip install scikit-learn>=0.21.3
-pip install xlrd >= 1.0.0
+cd ~/NBA_Pro_Line_Analytics/GBM_model_build/
 
-source deactivate gbmbuild
+pip install -r requirements.txt
